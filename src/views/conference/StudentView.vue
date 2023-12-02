f4f448cb7e782c2b85235f7be2156b52de74187c
Kazer0g <90910163+Kazer0g@users.noreply.github.com>
2023-12-02 20:30:21 +0300

    +StudentView

--- src/views/conference/StudentView.vue
+++ src/views/conference/StudentView.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- TODO: Write a conference view and logic -->
   <span>Welcome to the student view!</span>
+  <div class="container"></div>
   <!-- <editable-canvas-component />
   <static-canvas-component /> -->
 </template>
@@ -19,3 +20,37 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/main.scss';
+.container {
+  position: absolute;
+  display: flex;
+  top: 16px;
+  right: auto;
+  width: 580px;
+  height: 320px;
+  padding: {
+    left: 16px;
+    right: 16px;
+    top: 16px;
+    bottom: 8px;
+  };
+  border: {
+    radius: 16px;
+    width: 1px;    
+    style: solid;
+    color: var(--accent, #464AB4);
+  };
+  background: var(--background, #E5E6F5);
+  box-shadow: 2px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  flex-shrink: 0;
+
+  * {
+    width: 48px;
+    height: 48px;
+    
+    background-color: transparent;
+  }
+}
+</style>
