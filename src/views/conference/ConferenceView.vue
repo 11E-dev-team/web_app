8d648e5f84a05c40a7c441117cbe6c4da089f053
Zhur06 <zhur06@gmail.com>
2023-12-06 16:17:45 +0300

    ~ Edited ConferenceView in conference folder

--- src/views/conference/ConferenceView.vue
+++ src/views/conference/ConferenceView.vue
@@ -1,51 +1,70 @@
 <template>
-  <!-- TODO: Write a conference view and logic -->
-  <!-- <teachers-view v-if="isTeacher" /> -->
-  <teacher-view v-if=false />
-  <!-- <student-view v-else /> -->
-  <student-view2 v-else />
-  <!-- Switch back if else -->
+  <span>You're in {{ conferenceId }} conference</span>
+  <editable-canvas-component :fabricCanvas="fabricCanvas" />
+  <div v-for="fabricCanvas in fabricCanvasArray">
+    <static-canvas-component :fabricCanvas="fabricCanvas" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, Ref, ref } from 'vue';
 import { storeToRefs } from 'pinia';
 
 import { useUserStore } from '@/store';
-import { Conference } from '@/utils/canvasLogic/Conference';
-import StudentView from './StudentView.vue';
-import TeacherView from './TeacherView.vue';
-import StudentView2 from './StudentView2.vue';
+import Conference, { ClientConferenceEventTypes, IClientConferenceEvent } from '@/canvasLogic/Conference';
+import EditableCanvasComponent from '@/components/canvas/EditableCanvasComponent.vue';
+import StaticCanvasComponent from '@/components/canvas/StaticCanvasComponent.vue';
+import { FabricCanvas } from '@/canvasLogic/FabricCanvas';
 
 const userStore = useUserStore();
-const { conferenceId, idInConference } = storeToRefs(userStore);
+const { conferenceId, user } = storeToRefs(userStore);
 
 export default defineComponent({
   name: 'ConferenceView',
   components: {
-    StudentView,
-    TeacherView,
-    StudentView2
+    EditableCanvasComponent,
+    StaticCanvasComponent,
   },
-  // data() {
-  //   conferenceId.value = this.$route.params["id"] as string;
-  //   let conference: Conference | undefined;
-  //   return {
-  //     conferenceId,
-  //     conference,
-  //   }
-  // },
-  // mounted() {
-  //   this.conference = new Conference(this.conferenceId);
-  // },
-  // unmounted() {
-  //   this.conference?.leave();
-  // },
-  // computed: {
-  //   isTeacher(): boolean {
-  //     const idInConference_ = idInConference.value.find(x => x.conferenceId === this.conferenceId);
-  //     return idInConference_ !== undefined && idInConference_.role !== undefined && idInConference_.role >= 4;
-  //   },
-  // },
-})
+  data() {
+    conferenceId.value = this.$route.params["id"] as string;
+    let conference: Ref<Conference | undefined> = ref();
+    const fabricCanvasArray: Ref<FabricCanvas[]> = ref([]);
+
+    return {
+      conferenceId,
+      conference,
+      fabricCanvasArray,
+    }
+  },
+  mounted() {
+    this.conference = new Conference(this, this.conferenceId);
+  },
+  unmounted() {
+    this.conference?.leave();
+  },
+  methods: {
+    createCanvas(data: IClientConferenceEvent) {
+      const fabricCanvas = new FabricCanvas(data.data.id);
+      if (this.conference) fabricCanvas.conference = this.conference;
+      this.conference?.subscribe(fabricCanvas);
+      this.fabricCanvasArray.push(fabricCanvas);
+    },
+    update(data: IClientConferenceEvent) {
+      if (data.type === ClientConferenceEventTypes.CreateCanvas) {
+        this.createCanvas(data);
+      };
+    },
+  },
+  computed: {
+    fabricCanvas(): FabricCanvas | undefined {
+      return user.value?.id ? this.fabricCanvasArray[user.value?.id] : undefined;
+    },
+  },
+});
 </script>
+
+<style scoped lang="scss">
+div {
+  display: none;
+}
+</style>
